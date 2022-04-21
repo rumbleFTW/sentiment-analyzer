ad8e76e3c45331bd085c468308abe7434783b1bf
rumbleFTW <007rajdeepghosh@gmail.com>
2022-04-21 20:46:48 +0530

    Improved the styling

--- src/Panel.js
+++ src/Panel.js
@@ -4,8 +4,9 @@ function Panel() {
   return (
     <div className='panel--outer'>
       <div className='panel--inner'>
+        <h1>Sentiment-Analyzer</h1>
         <form>
-          <input className='textfield'></input>
+          <textarea className='textfield' name='Text' cols="50" rows="10" required></textarea>
           <button className='button'>Submit</button>
         </form>
       </div>
