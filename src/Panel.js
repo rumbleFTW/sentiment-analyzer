4bcdc7b44437ca119cab35bb5ac5f41ec489c8dd
rumbleFTW <007rajdeepghosh@gmail.com>
2022-04-22 05:17:53 +0530

    Added the notebook for training the model

--- src/Panel.js
+++ src/Panel.js
@@ -1,10 +1,13 @@
 import './style.css';
+import * as tf from '@tensorflow/tfjs';
 
 function Panel() {
+
+  const model = await tf.loadLayersModel("C:\\Users\\007ra\\Documents\\Codes\\AI-ML\\sentiment-analyzer\\AI\\model.json");
   return (
     <div className='panel--outer'>
       <div className='panel--inner'>
-        <h1>Sentiment-Analyzer</h1>
+        <h1 className='title'>SENTIMENT-ANALYZER</h1>
         <form>
           <textarea className='textfield' name='Text' cols="50" rows="10" required></textarea>
           <button className='button'>Submit</button>
