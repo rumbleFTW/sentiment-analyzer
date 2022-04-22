fc944c23c20ef4f36e7eb599f6c9366d9f402318
rumbleFTW <007rajdeepghosh@gmail.com>
2022-04-22 14:13:00 +0530

    Backend almost ready!

--- src/Panel.js
+++ src/Panel.js
@@ -1,16 +1,51 @@
 import './style.css';
+// import React, { Component }  from 'react';
 import * as tf from '@tensorflow/tfjs';
+import tokenizer from './tokenizer.json'
 
 function Panel() {
 
-  const model = await tf.loadLayersModel("C:\\Users\\007ra\\Documents\\Codes\\AI-ML\\sentiment-analyzer\\AI\\model.json");
+  function getTokenisedWord(seedWord) {
+    const _token = tokenizer[seedWord.toLowerCase()]
+    return _token
+  }
+
+  function textToSequence(sentence)
+  {
+    var res = [];
+    for (var i = 0; i<sentence.length; i++)
+    {
+      res.push(getTokenisedWord(sentence[i]))
+    }
+    return res
+  }
+
+  function pad(sequence)
+  {
+    var res = []
+    const maxlen = 66;
+    for(var i = 0; i<maxlen-sequence.length; i++)
+    {
+      res.push(0)
+    }
+    res = res.concat(sequence)
+    return tf.tensor2d([res])
+  }
+
+  async function analyze()
+  {
+    const model = await tf.loadLayersModel('model.json')
+    var pred = model.predict([pad(textToSequence(['my', 'sister', 'died', 'of', 'covid', 'yesterday']))])
+    console.log(pred.array())
+  }
+
   return (
     <div className='panel--outer'>
       <div className='panel--inner'>
         <h1 className='title'>SENTIMENT-ANALYZER</h1>
         <form>
           <textarea className='textfield' name='Text' cols="50" rows="10" required></textarea>
-          <button className='button'>Submit</button>
+          <button className='button' onClick={analyze}>Submit</button>
         </form>
       </div>
     </div>
