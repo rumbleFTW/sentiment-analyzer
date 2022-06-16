199bc9a384ff8d6b3f1ff8f01e964829f5738f74
Rajdeep Ghosh <85807431+rumbleFTW@users.noreply.github.com>
2022-06-16 09:23:34 +0530

    Added Emoji Rain

--- src/Panel.js
+++ src/Panel.js
@@ -49,6 +49,72 @@ function Panel() {
     const ans = Array.from(pred.dataSync()).indexOf(Math.max(...Array.from(pred.dataSync())))
     const dictEmotion = {0: 'joy', 1: 'fear', 2: 'anger', 3: 'sadness', 4: 'disgust', 5: 'shame' , 6: 'guilt'}
     const dictEmoji = {0: ['😃', '😄', '😁 '], 1: ['😨', '😬', '😱'], 2: ['😠', '💢', '😤'], 3: ['😥', '😭', '☹️'], 4: ['🤢', '💩', '🤮'], 5: ['😖', '😣', '😫'], 6: ['😅', '😓', '🥴']}
+    var renderArray = [dictEmoji[ans]];
+    var container = document.getElementById('root');
+    var emoji = ['🌽', '🍇', '🍌', '🍒', '🍕', '🍷', '🍭', '💖', '💩', '🐷', '🐸', '🐳', '🎃', '🎾', '🌈', '🍦', '💁', '🔥', '😁', '😱', '🌴', '👏', '💃'];
+    var circles = [];
+
+    for (var i = 0; i < 15; i++) {
+      addCircle(i * 150, [10 + 0, 300], emoji[Math.floor(Math.random() * emoji.length)]);
+      addCircle(i * 150, [10 + 0, -300], emoji[Math.floor(Math.random() * emoji.length)]);
+      addCircle(i * 150, [10 - 200, -300], emoji[Math.floor(Math.random() * emoji.length)]);
+      addCircle(i * 150, [10 + 200, 300], emoji[Math.floor(Math.random() * emoji.length)]);
+      addCircle(i * 150, [10 - 400, -300], emoji[Math.floor(Math.random() * emoji.length)]);
+      addCircle(i * 150, [10 + 400, 300], emoji[Math.floor(Math.random() * emoji.length)]);
+      addCircle(i * 150, [10 - 600, -300], emoji[Math.floor(Math.random() * emoji.length)]);
+      addCircle(i * 150, [10 + 600, 300], emoji[Math.floor(Math.random() * emoji.length)]);
+    }
+
+
+
+    function addCircle(delay, range, color) {
+      setTimeout(function() {
+        var c = new Circle(range[0] + Math.random() * range[1], 80 + Math.random() * 4, color, {
+          x: -0.15 + Math.random() * 0.3,
+          y: 1 + Math.random() * 1
+        }, range);
+        circles.push(c);
+      }, delay);
+    }
+
+    function Circle(x, y, c, v, range) {
+      var _this = this;
+      this.x = x;
+      this.y = y;
+      this.color = c;
+      this.v = v;
+      this.range = range;
+      this.element = document.createElement('span');
+      /*this.element.style.display = 'block';*/
+      this.element.style.opacity = 0;
+      this.element.style.position = 'absolute';
+      this.element.style.fontSize = '26px';
+      this.element.style.color = 'hsl('+(Math.random()*360|0)+',80%,50%)';
+      this.element.innerHTML = c;
+      container.appendChild(this.element);
+
+      this.update = function() {
+        if (_this.y > 800) {
+          _this.y = 80 + Math.random() * 4;
+          _this.x = _this.range[0] + Math.random() * _this.range[1];
+        }
+        _this.y += _this.v.y;
+        _this.x += _this.v.x;
+        this.element.style.opacity = 1;
+        this.element.style.transform = 'translate3d(' + _this.x + 'px, ' + _this.y + 'px, 0px)';
+        this.element.style.webkitTransform = 'translate3d(' + _this.x + 'px, ' + _this.y + 'px, 0px)';
+        this.element.style.mozTransform = 'translate3d(' + _this.x + 'px, ' + _this.y + 'px, 0px)';
+      };
+    }
+
+    function animate() {
+      for (var i in circles) {
+        circles[i].update();
+      }
+      requestAnimationFrame(animate);
+    }
+
+    animate();
     console.log(dictEmotion[ans])
   }
 
